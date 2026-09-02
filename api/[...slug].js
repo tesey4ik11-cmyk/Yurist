ed0108deca64bd26f7bce1a99f3aacd145af6f0e
tesey4ik11-cmyk <tesey4ik11@gmail.com>
2026-09-02 16:53:59 +0300

    test: Node.js (req, res) handler style

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,7 +1,6 @@
-module.exports = async function handler(req) {
-  const url = new URL(req.url);
-  return new Response(JSON.stringify({ ok: true, path: url.pathname }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
-  });
+module.exports = async function handler(req, res) {
+  const url = new URL(req.url, 'http://localhost');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({ ok: true, path: url.pathname });
 };
